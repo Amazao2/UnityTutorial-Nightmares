9009b94f26a0830f340d9423509593792f729978
Brandon McRae <bmcrae2@uwo.ca>
2014-10-07 16:40:57 -0400

    Phase 3
    - setup orthographic camera
    - adds script to make the main camera follow the player

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smoothing = 5f; // adds a bit of lag when the camera follows the player
+
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+
+    }
+
+    // FixedUpdate is used to follow a rigid body (same as the player's update)
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime); // Lerp slowly moves between 2 positions
+    }
+
+}
