bf75cd6892d6c48685ba9a438e3ddbff36f5c2ef
Brandon McRae <bmcrae2@uwo.ca>
2014-10-08 00:42:22 -0400

    Shotgun
    - adds the shotgun functionality
    - press 1 for rifle, 2 for shotgun
    - adds UI element for rifle and shotgun keys
    - adjusts damage on weapons, health of enemies and enemy frequency for
    balance

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerShooting : MonoBehaviour
 {
-    public int damagePerShot = 20;
-    public float timeBetweenBullets = 0.15f;
-    public float range = 100f;
+    public float lineWidthOverride = 0.05f;
+    public bool isShotgun = false;
+
+    // Rifle properties
+    public int rifleDamage = 20;
+    public float rifleReload = 0.15f;
+    public float rifleRange = 100f;
+
+    // Shotgun Properties
+    public int shotgunDamage = 1;
+    public float shotgunReload = 0.5f;
+    public float shotgunRange = 30f;    
 
 
     float timer;
-    Ray shootRay;
     RaycastHit shootHit;
     int shootableMask;
     ParticleSystem gunParticles;
-    LineRenderer gunLine;
+    List<LineRenderer> gunLines;
+    LineRenderer baseLineRenderer;
     AudioSource gunAudio;
     Light gunLight;
     float effectsDisplayTime = 0.2f;
@@ -22,7 +32,8 @@ public class PlayerShooting : MonoBehaviour
     {
         shootableMask = LayerMask.GetMask ("Shootable");
         gunParticles = GetComponent<ParticleSystem> ();
-        gunLine = GetComponent <LineRenderer> ();
+        gunLines = new List<LineRenderer>();
+        baseLineRenderer = GetComponent<LineRenderer>();
         gunAudio = GetComponent<AudioSource> ();
         gunLight = GetComponent<Light> ();
     }
@@ -31,6 +42,20 @@ public class PlayerShooting : MonoBehaviour
     void Update ()
     {
         timer += Time.deltaTime;
+
+        // switch guns
+        if ( Input.GetKeyDown(KeyCode.Alpha1) ) // rifle
+            isShotgun = false;
+        else if ( Input.GetKeyDown(KeyCode.Alpha2) ) // shotgun)
+            isShotgun = true;
+
+        float timeBetweenBullets;
+        
+        // what gun is selected?
+        if (isShotgun)
+            timeBetweenBullets = shotgunReload; 
+        else
+            timeBetweenBullets = rifleReload;
 
         if(Input.GetButton ("Fire1") && timer >= timeBetweenBullets)
         {
@@ -46,7 +71,11 @@ public class PlayerShooting : MonoBehaviour
 
     public void DisableEffects ()
     {
-        gunLine.enabled = false;
+        gunLines.ForEach(delegate(LineRenderer l)
+        {
+            Destroy(l.gameObject);
+        });
+        gunLines.Clear();
         gunLight.enabled = false;
     }
 
@@ -59,27 +88,86 @@ public class PlayerShooting : MonoBehaviour
 
         gunLight.enabled = true;
 
-        gunParticles.Stop ();
-        gunParticles.Play ();
+        gunParticles.Stop();
+        gunParticles.Play();
 
-        gunLine.enabled = true;
-        gunLine.SetPosition (0, transform.position);
-
-        shootRay.origin = transform.position;
-        shootRay.direction = transform.forward;
-
-        if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+        if (isShotgun)
         {
-            EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
-            if(enemyHealth != null)
+            ShotgunShot().ForEach(delegate(Ray r)
             {
-                enemyHealth.TakeDamage (damagePerShot, shootHit.point);
-            }
-            gunLine.SetPosition (1, shootHit.point);
+                SingleShot(r);
+            });
         }
         else
         {
-            gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+            SingleShot(CreateRay(transform.forward));
         }
+
+        
+    }
+
+    
+
+    List<Ray> ShotgunShot()
+    {
+        List<Ray> bullets = new List<Ray>();
+
+        bullets.Add(CreateRay(transform.forward));
+
+        for (int i = -60; i <= 60; i++)
+        {
+            bullets.Add(CreateRay(Quaternion.AngleAxis(i/2.0f, Vector3.up) * transform.forward));
+        }            
+
+        return bullets;
+    }
+
+    void SingleShot(Ray ray)
+    {
+        LineRenderer gunLine = new GameObject().AddComponent<LineRenderer>();
+        gunLine.enabled = true;
+        gunLine.materials = baseLineRenderer.materials;
+        gunLine.SetWidth(lineWidthOverride, lineWidthOverride);
+        gunLine.SetPosition(0, transform.position);
+
+        gunLines.Add(gunLine);
+
+        float range;
+        int damagePerShot;
+
+        if (isShotgun)
+        {
+            range = shotgunRange;
+            damagePerShot = shotgunDamage;
+        }
+        else
+        {
+            range = rifleRange;
+            damagePerShot = rifleDamage;
+        }
+
+        if (Physics.Raycast(ray, out shootHit, range, shootableMask))
+        {
+            EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damagePerShot, shootHit.point);
+            }
+            gunLine.SetPosition(1, shootHit.point);
+        }
+        else
+        {
+            gunLine.SetPosition(1, ray.origin + ray.direction * range);
+        }
+    }
+
+    Ray CreateRay(Vector3 direction)
+    {
+        var ray = new Ray();
+
+        ray.origin = transform.position;
+        ray.direction = direction;
+
+        return ray;
     }
 }
