62675650e14caa09b059197b42fe162e09dd872e
Brandon McRae <bmcrae2@uwo.ca>
2014-10-07 22:25:22 -0400

    God Mode
    - toggle god mode by pressing g
    - removes capsule collider so you can run anywhere and not die

---
+++ Assets/Scripts/Player/GodMode.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GodMode : MonoBehaviour {
+
+    public CapsuleCollider shield;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if( shield.enabled )
+            {
+                shield.enabled = false;
+            }
+            else
+            {
+                shield.enabled = true;
+            }
+            
+        }
+    }
+}
