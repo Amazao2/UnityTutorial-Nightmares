cc255692b091546192871bbd121ac55597972a96
Brandon McRae <bmcrae2@uwo.ca>
2014-10-08 01:54:29 -0400

    Godmode - Weapon Speed Increase
    - adds decreased reload time when in godmode

--- Assets/Scripts/Player/GodMode.cs
+++ Assets/Scripts/Player/GodMode.cs
@@ -4,6 +4,23 @@ using System.Collections;
 public class GodMode : MonoBehaviour {
 
     public CapsuleCollider shield;
+    public Transform gunBarrelEnd;
+
+    public float godlikeShotgunReload = 0.25f;
+    public float godlikeRifleReload = 0.20f;
+
+    PlayerShooting weapons;
+
+    float defaultShotgunReload;
+    float defaultRifleReload;
+
+    void Start()
+    {
+        weapons = gunBarrelEnd.GetComponent<PlayerShooting>();
+
+        defaultShotgunReload = weapons.shotgunReload;
+        defaultRifleReload = weapons.shotgunReload;
+    }
 
     void FixedUpdate()
     {
@@ -12,10 +29,14 @@ public class GodMode : MonoBehaviour {
             if( shield.enabled )
             {
                 shield.enabled = false;
+                weapons.shotgunReload = godlikeShotgunReload;
+                weapons.rifleReload = godlikeRifleReload;
             }
             else
             {
                 shield.enabled = true;
+                weapons.shotgunReload = defaultShotgunReload;
+                weapons.rifleReload = defaultRifleReload;
             }
             
         }
